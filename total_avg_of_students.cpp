db13ed343ede99ce7a8538b020a3ad3a57022ece
Vandrangi.Rishita Bhargavi <vrishitabhargavi@gmail.com>
2026-02-17 13:46:11 +0530

    Add files via upload

---
+++ total_avg_of_students.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+struct student
+{
+	int rno;
+	int m1,m2,m3;
+	int total,average;
+};
+int main()
+{
+	int n,i;
+	struct student s[60];
+	printf("Enter number of students\n");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		printf("student %d details",i+1);
+		scanf("%d%d%d%d",&s[i].rno,&s[i].m1,&s[i].m2,&s[i].m3);
+		s[i].total=s[i].m1+s[i].m2+s[i].m3;
+		s[i].average=s[i].total/3;
+	}
+	printf("R.no Total Average\n");
+	for(i=0;i<n;i++)
+	{
+		printf("%d    %d    %d\n",s[i].rno,s[i].total,s[i].average);
+	}
+	return 0;
+}
+
